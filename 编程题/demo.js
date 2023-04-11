58df30415f6ca4a45047f521cfb3d556f99a8f21
linzhi <48920243+linzhi-linzhi@users.noreply.github.com>
2023-04-11 19:43:19 +0800

    每隔一端时间执行一次函数，一共执行n次

--- 编程题/demo.js
+++ 编程题/demo.js
@@ -212,6 +212,30 @@ function arrayToTree(items) {
 
 
 
+/**
+ *   每隔一端时间执行一次函数，一共执行n次
+ **/
+
+// 实现一个函数，使得 repeat(() => {console.log('1')}, 5, 2000) 每两秒执行一次打印，总共五次
+
+function wait(millisecond) {
+    return new Promise((resovle) => {
+        setTimeout(() => {
+            resovle()
+        }, millisecond);
+    })
+}
+
+async function repeat(task, count = 1, millisecond = 0) {
+    while (count--) {
+        await wait(millisecond)
+        task()
+    }
+}
+
+repeat(() => {
+    console.log('1')
+}, 5, 2000)
 
 
 
